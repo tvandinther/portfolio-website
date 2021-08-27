9596b5726abb0ad62e630c9577be0164b2bdbe7c
tvandinther <tvandinther@gmail.com>
2021-08-27 14:34:40 +1200

    playing with 3d

--- src/threeScript.js
+++ src/threeScript.js
@@ -1,78 +1,97 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import modelUrl from './assets/abstract_ball/scene.gltf?url'
+// import modelUrl from './assets/tropical_island/scene.glb?url'
+
+// import { BloomEffect, EffectComposer, EffectPass, RenderPass, SMAAEffect } from "postprocessing";
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js';
 
 export function init() {
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(255, 255, 255);
-    scene.fog = new THREE.FogExp2(0xffffff, 0.025);
+    // scene.background = new THREE.Color(255, 255, 255);
+    scene.fog = new THREE.FogExp2(0xf3f3f3, 0.025);
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     const renderer = new THREE.WebGLRenderer({
         canvas: document.querySelector("#bg"),
+        powerPreference: "high-performance",
+        antialias: false,
+        stencil: false,
+        depth: false
     });
 
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    camera.position.set(-3, 40, 15);
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+    composer.addPass(new ShaderPass(CopyShader))
+
+    camera.position.set(0, 3, 0);
 
     renderer.render(scene, camera);
 
-    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-    const material = new THREE.MeshStandardMaterial({ color: 0x0262b6 });
-    const torus = new THREE.Mesh(geometry, material);
-    torus.position.set(30, 5, -15);
+    const loader = new GLTFLoader();
+    let model = new THREE.Group();
 
-    scene.add(torus);
+    loader.load( modelUrl, function ( gltf ) {
+        model.add( gltf.scene );
 
-    const pointLight = new THREE.PointLight(0x0262b6);
-    pointLight.position.set(0, 0, 0);
-    scene.add(pointLight);
+    }, undefined, function ( error ) {
 
-    const ambientLight = new THREE.AmbientLight(0xffffff);
-    scene.add(pointLight, ambientLight);
+        console.error( error );
+
+    } );
+
+    scene.add(model);
+
+    const spotLight = new THREE.SpotLight(0xFDF4FF , 1, 50, Math.PI / 8, 1);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+
+    spotLight.position.set(0, -1, 1);
+    spotLight.target = model;
+    camera.add(spotLight);
+    scene.add(camera);
+
+    // const ambientLight = new THREE.AmbientLight(0xffffff);
+    // scene.add(ambientLight);
 
     const controls = new OrbitControls(camera, renderer.domElement);
-
-    function addStar() {
-        const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-        const material = new THREE.MeshStandardMaterial({ color: 0x0262b6 });
-        const star = new THREE.Mesh(geometry, material);
-
-        const [x, y, z] = Array(3)
-            .fill()
-            .map(() => THREE.MathUtils.randFloatSpread(100));
-
-        star.position.set(x, y, z);
-        scene.add(star);
-    }
-
-    Array(200).fill().forEach(addStar);
 
     function moveCamera() {
         let topOffset = document.body.getBoundingClientRect().top;
 
         topOffset += -1;
 
-        camera.position.z = topOffset * -0.1;
-        camera.position.x = topOffset * -0.1;
-        camera.rotation.y = topOffset * -0.1;
+        model.setRotationFromAxisAngle(new THREE.Vector3(0.54, 0.31, 0.84), topOffset * -0.001);
+
+        // camera.position.z = topOffset * -0.1;
+        // camera.position.x = topOffset * -0.1;
+        // camera.rotation.y = topOffset * -0.1;
     }
 
     document.body.onscroll = moveCamera;
     moveCamera();
 
+    const clock = new THREE.Clock();
+
     function animate() {
         requestAnimationFrame(animate);
 
-        torus.rotation.x += 0.01;
-        torus.rotation.y += 0.005;
-        torus.rotation.z += 0.01;
-
         controls.update();
 
-        renderer.render(scene, camera);
+        composer.render(clock.getDelta());
     }
 
     animate();
